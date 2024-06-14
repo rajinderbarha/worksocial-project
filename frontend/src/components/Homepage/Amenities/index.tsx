fd40139c501f58f3c9e85cb10b56bc0fcbc77975
rajinderbarha <rajinderbarha@gmail.com>
2024-06-14 17:00:02 +0530

    added fonts

--- frontend/src/components/Homepage/Amenities/index.tsx
+++ frontend/src/components/Homepage/Amenities/index.tsx
@@ -1,35 +1,29 @@
-import React from 'react'
-import classes from './workbetter.module.css'
-import Image from 'next/image'
+import React from 'react';
+import classes from './workbetter.module.css';
+import Image from 'next/image';
 import { getStrapiMedia } from '@/data/utils';
+
 function Amenities({ data }: any) {
-    console.log(data);
-    const { title, ammenities} = data
-  return (
-    <div className={`${classes.Workbetter} pink`}>
-      <h2 className='text-center'>{title}</h2>
-      <div className={`${classes.Workbetter_Icon_boxs}`}>
-
-        <div className='row '>
-          {ammenities.data.map((ammenities: any, index: number) => (
-              
-          <div key={index} className='col-md-3 col-6 px-1 mb-2'>
-            <div className={`${classes.Workbetter_boxs} ${classes.teal} text-center rounded_border `}>
-              <Image src={getStrapiMedia(ammenities.Icon.url)} alt='WiFi' className='mx-auto' width={160} height={160}/>
-              <p>{ammenities.title}</p>
+    const { title, ammenities } = data;
+    return (
+        <div className={`${classes.Workbetter} pink`}>
+            <h2 className='text-center'>{title}</h2>
+            <div className={`${classes.Workbetter_Icon_boxs}`}>
+                <div className='row'>
+                    {ammenities.data.map((ammenities: any, index: number) => (
+                        <div key={index} className='col-md-3 col-6 px-1 mb-2'>
+                            <div className={`${classes.Workbetter_boxs} ${index % 2 === 0 ? classes.radius117 : classes.radius30} text-center`}>
+                                <Image src={getStrapiMedia(ammenities.Icon.url)} alt={ammenities.title} className='mx-auto' width={100} height={100} />
+                                <div className={classes.Workbetter_boxs_P}>
+                                    <p>{ammenities.title}</p>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
             </div>
-          </div>
-          ))}
-
-
-          
-
-          
         </div>
-
-      </div>
-    </div>
-  )
+    );
 }
 
-export default Amenities
+export default Amenities;
--- frontend/src/components/Homepage/BrandsLogo/index.tsx
+++ frontend/src/components/Homepage/BrandsLogo/index.tsx
@@ -1,30 +1,28 @@
-import React from 'react'
-import classes from './brandslogos.module.css'
-import Image from 'next/image'
+import React from 'react';
+import classes from './brandslogos.module.css';
+import Image from 'next/image';
 import { getStrapiMedia } from '@/data/utils';
+
 function Brandslogos({ data }: any) {
-    const {title, description, brand_collaborations } = data
-    
+    const { title, description, brand_collaborations } = data;
+
     return (
-        <div className={`${classes.Brandslogos} `}>
+        <div className={classes.Brandslogos}>
             <div className="Containers_x">
                 <h2>{title}</h2>
                 <p>{description}</p>
-
             </div>
             <div className="Containers_main">
-                
-                <div className={`${classes.Brandslogos_box}`}>
+                <div className={classes.Brandslogos_box}>
                     {brand_collaborations.data.map((item: any, index: number) => (
-
-                        <Image key={index} src={getStrapiMedia(item.BrandLogo.url)} alt={item.BrandName} width={81} height={53}/>
-                        
+                        <div key={index}>
+                            <Image src={getStrapiMedia(item.BrandLogo.url)} alt={item.BrandName} width={50} height={50} />
+                        </div>
                     ))}
-                      
                 </div>
             </div>
         </div>
-    )
+    );
 }
 
-export default Brandslogos
+export default Brandslogos;
